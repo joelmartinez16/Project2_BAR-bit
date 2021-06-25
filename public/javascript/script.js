3e5b71a639033a7ad0614e62665a671bc6fb1418
Joel Martinez <joemartinez87@hotmail.com>
2021-06-24 18:26:53 -0700

    creating the javascript for the google map

---
+++ public/javascript/script.js
@@ -0,0 +1,44 @@
+// APIkey=  AIzaSyD3yGy9fQWmrjGT3ha2zwjHwiBUXI09-mE
+{/* <script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyD3yGy9fQWmrjGT3ha2zwjHwiBUXI09-mE&callback=initMap"
+type="text/javascript"></script> */}
+{/* <script async
+    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyD3yGy9fQWmrjGT3ha2zwjHwiBUXI09-mE&libraries=geometry,places&callback=initMap">
+</script> */}
+
+
+
+  function initMap() {
+    const city = {
+        center: {lat: 32.7157, lng: -117.1611},
+        zoom:15
+    } 
+    map = new google.maps.Map(document.getElementById('map');
+    
+    const marker = new google.maps.Marker({
+      position: {lat: 32.7157, lng: -117.1611},
+      map: map
+    });
+  }
+  
+  getApi(apiKey);
+  
+
+
+
+
+  const mon = [
+      [ "C Level"<br> "880 Harbor Island Dr, San Diego, CA 92101"<br>
+      <a href="https://www.yelp.com/biz/c-level-san-diego">Get info</a>]
+      
+      ['Belgian Beer & Waffle'<br> '2820 Historic Decatur Rd San Diego, CA 92106'<br>
+        <a href= "https://www.yelp.com/biz/belgian-beer-and-waffle-san-diego-2">Get info</a>]
+
+      ["Bud’s Louisiana Cafe" <br>"4320 Viewridge Ave Ste A San Diego, CA 92123"<br>
+        <a href= "https://www.yelp.com/biz/buds-louisiana-cafe-san-diego?osq=Bud%27s+Louisiana+Cafe">Get info</a>]
+
+      ["Kogi BBQ"<br>"5447 Kearny Villa Rd Ste A San Diego, CA 92123"<br>
+        <a href="https://www.yelp.com/biz/kogi-bbq-san-diego?osq=Kogi+BBQ">Get info</a>] 
+    
+      ["Muzita Abyssinian Bistro"<br> "4651 Park Blvd San Diego, CA 92116"<br>
+        <a href="https://www.yelp.com/biz/muzita-abyssinian-bistro-san-diego?osq=Muzita+Abyssinian+Bistro">Get info</a>]
+  ];
