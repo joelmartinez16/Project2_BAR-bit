cb45de0bb8646212e523ef925eb7c4c00a452e02
Amalec Guzman <amalec.c.guzman@gmail.com>
2021-06-24 21:42:48 -0700

    Added data information for the seeds and finished the sequelized .

---
+++ Seeds/BarData.js
@@ -0,0 +1,325 @@
+const { Bar } = require('../Bar-Models');
+
+const BarData = [  
+    
+    { 
+        bars_name:"C Level", 
+        rating: '4.5 stars',
+        pricing: '$$',
+        hours: 'Sun- Thurs: 11:30 AM- 9:00 PM, Fri & Sat: 11:30 AM- 9:30 PM;', 
+        reservation: 'Takes Reservations',
+        review: ' Great place for a celebration for any occasion. Beautiful view, great service, and good food.',
+        day_of_week: 'Moody Monday',
+     }, 
+
+     { 
+         bars_name:"",
+         rating:"",
+         pricing:"", 
+         hours:"", 
+         reservation:"",
+         review:"", 
+         day_of_week:"",
+
+     }, 
+        
+     {
+        bars_name:"",
+        rating:"",
+        pricing:"", 
+        hours:"", 
+        reservation:"",
+        review:""
+     }, 
+
+     {
+        bars_name:"",
+        rating:"",
+        pricing:"", 
+        hours:"", 
+        reservation:"",
+        review:""
+     }, 
+
+     {
+        bars_name:"",
+        rating:"",
+        pricing:"", 
+        hours:"", 
+        reservation:"",
+        review:""
+     }, 
+
+     {
+        bars_name:"",
+        rating:"",
+        pricing:"", 
+        hours:"", 
+        reservation:"",
+        review:""
+     }, 
+
+     {
+        bars_name:"",
+        rating:"",
+        pricing:"", 
+        hours:"", 
+        reservation:"",
+        review:""
+     }, 
+
+     {
+        bars_name:"",
+        rating:"",
+        pricing:"", 
+        hours:"", 
+        reservation:"",
+        review:""
+     },
+
+     {
+        bars_name:"",
+        rating:"",
+        pricing:"", 
+        hours:"", 
+        reservation:"",
+        review:""
+     }, 
+
+     {
+        bars_name:"",
+        rating:"",
+        pricing:"", 
+        hours:"", 
+        reservation:"",
+        review:""
+     }, 
+
+     {
+        bars_name:"",
+        rating:"",
+        pricing:"", 
+        hours:"", 
+        reservation:"",
+        review:""
+     }, 
+
+     {
+        bars_name:"",
+        rating:"",
+        pricing:"", 
+        hours:"", 
+        reservation:"",
+        review:""
+     }, 
+
+     {
+        bars_name:"",
+        rating:"",
+        pricing:"", 
+        hours:"", 
+        reservation:"",
+        review:""
+     },  
+
+     {
+        bars_name:"",
+        rating:"",
+        pricing:"", 
+        hours:"", 
+        reservation:"",
+        review:""
+     },  
+
+     {
+        bars_name:"",
+        rating:"",
+        pricing:"", 
+        hours:"", 
+        reservation:"",
+        review:""
+     },  
+
+     {
+        bars_name:"",
+        rating:"",
+        pricing:"", 
+        hours:"", 
+        reservation:"",
+        review:""
+     },  
+
+     {
+        bars_name:"",
+        rating:"",
+        pricing:"", 
+        hours:"", 
+        reservation:"",
+        review:""
+     },  
+
+     {
+        bars_name:"",
+        rating:"",
+        pricing:"", 
+        hours:"", 
+        reservation:"",
+        review:""
+     },  
+
+     {
+        bars_name:"",
+        rating:"",
+        pricing:"", 
+        hours:"", 
+        reservation:"",
+        review:""
+     },  
+
+     {
+        bars_name:"",
+        rating:"",
+        pricing:"", 
+        hours:"", 
+        reservation:"",
+        review:""
+     },  
+
+     {
+        bars_name:"",
+        rating:"",
+        pricing:"", 
+        hours:"", 
+        reservation:"",
+        review:""
+     },  
+
+     {
+        bars_name:"",
+        rating:"",
+        pricing:"", 
+        hours:"", 
+        reservation:"",
+        review:""
+     },  
+
+     {
+        bars_name:"",
+        rating:"",
+        pricing:"", 
+        hours:"", 
+        reservation:"",
+        review:""
+     },  
+
+     {
+        bars_name:"",
+        rating:"",
+        pricing:"", 
+        hours:"", 
+        reservation:"",
+        review:""
+     },  
+
+     {
+        bars_name:"",
+        rating:"",
+        pricing:"", 
+        hours:"", 
+        reservation:"",
+        review:""
+     },  
+     {
+        bars_name:"",
+        rating:"",
+        pricing:"", 
+        hours:"", 
+        reservation:"",
+        review:""
+     }, 
+     
+     {
+        bars_name:"",
+        rating:"",
+        pricing:"", 
+        hours:"", 
+        reservation:"",
+        review:""
+     },  
+
+     {
+        bars_name:"",
+        rating:"",
+        pricing:"", 
+        hours:"", 
+        reservation:"",
+        review:""
+     },  
+
+     {
+        bars_name:"",
+        rating:"",
+        pricing:"", 
+        hours:"", 
+        reservation:"",
+        review:""
+     },  
+
+     {
+        bars_name:"",
+        rating:"",
+        pricing:"", 
+        hours:"", 
+        reservation:"",
+        review:""
+     },  
+
+     {
+        bars_name:"",
+        rating:"",
+        pricing:"", 
+        hours:"", 
+        reservation:"",
+        review:""
+     },  
+
+     {
+        bars_name:"",
+        rating:"",
+        pricing:"", 
+        hours:"", 
+        reservation:"",
+        review:""
+     },  
+
+     {
+        bars_name:"",
+        rating:"",
+        pricing:"", 
+        hours:"", 
+        reservation:"",
+        review:""
+     }, 
+
+     {
+        bars_name:"",
+        rating:"",
+        pricing:"", 
+        hours:"", 
+        reservation:"",
+        review:""
+     },  
+
+     {
+        bars_name:"",
+        rating:"",
+        pricing:"", 
+        hours:"", 
+        reservation:"",
+        review:""
+     }, 
+
+
+
+
+]
