065e440791ad69c7d484f9bb06469d6f0be3c684
Quoc Ha <qhaa190@gmail.com>
2021-06-28 15:53:14 -0700

    my updates

---
+++ Api Routes/user-routes.js
@@ -0,0 +1,35 @@
+const router = require('express').Router();
+const { User } = require('../../models');
+
+// CREATE new user
+router.post('/', async (req, res) => {
+  try {
+    const dbUserData = await User.create({
+      username: req.body.username,
+      email: req.body.email,
+      password: req.body.password,
+    });
+
+    // Set up sessions with a 'loggedIn' variable set to `true`
+    req.session.save(() => {
+      req.session.loggedIn = true;
+
+      res.status(200).json(dbUserData);
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+// Logout
+router.post('/logout', (req, res) => {
+    // When the user logs out, destroy the session
+    if (req.session.loggedIn) {
+      req.session.destroy(() => {
+        res.status(204).end();
+      });
+    } else {
+      res.status(404).end();
+    }
+  });
