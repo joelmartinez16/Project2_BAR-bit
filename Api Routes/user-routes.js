c9cc9404ac9295066d8fb4727c57d3df6adf8d4f
Quoc Ha <qhaa190@gmail.com>
2021-06-28 16:48:27 -0700

    updates

--- Api Routes/user-routes.js
+++ Api Routes/user-routes.js
@@ -22,6 +22,45 @@ router.post('/', async (req, res) => {
   }
 });
 
+// Login
+router.post('/login', async (req, res) => {
+    try {
+      const dbUserData = await User.findOne({
+        where: {
+          email: req.body.email,
+        },
+      });
+  
+      if (!dbUserData) {
+        res
+          .status(400)
+          .json({ message: 'Incorrect email or password. Please try again!' });
+        return;
+      }
+  
+      const validPassword = await dbUserData.checkPassword(req.body.password);
+  
+      if (!validPassword) {
+        res
+          .status(400)
+          .json({ message: 'Incorrect email or password. Please try again!' });
+        return;
+      }
+  
+      // Once the user successfully logs in, set up the sessions variable 'loggedIn'
+      req.session.save(() => {
+        req.session.loggedIn = true;
+  
+        res
+          .status(200)
+          .json({ user: dbUserData, message: 'You are now logged in!' });
+      });
+    } catch (err) {
+      console.log(err);
+      res.status(500).json(err);
+    }
+  });
+
 // Logout
 router.post('/logout', (req, res) => {
     // When the user logs out, destroy the session
